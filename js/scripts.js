fb9a22610af2c2b5fa12bb6c43938798162a667e
kevinprosise <kevin.prosise@gmail.com>
2021-04-30 20:07:43 -0500

    Added Toggle Menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi! Welcome to My Portfolio Site!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += 'responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
