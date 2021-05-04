037039cedc74c4821245105c376c7c398fe20496
kevinprosise <kevin.prosise@gmail.com>
2021-05-03 21:29:27 -0500

    Edited Space

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ console.log("Hi! Welcome to My Portfolio Site!");
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
